8e6dd82c1c7936290ba364cb935cc66ffd56122c
Vraj Soni <vrajds8725@gmail.com>
2022-12-26 15:17:10 +0530

    added HeapSort

--- src/main/java/io/dsa/sorting/HeapSort.java
+++ src/main/java/io/dsa/sorting/HeapSort.java
@@ -1,4 +1,26 @@
 package io.dsa.sorting;
 
+import java.util.PriorityQueue;
+
+import static io.dsa.sorting.SortingUtils.printArr;
+import static io.dsa.sorting.SortingUtils.randomNumbersArr;
+
 public class HeapSort {
+
+    public static void main(String[] args) {
+        int[] arr = randomNumbersArr(20);
+        printArr(arr);
+        heapSort(arr);
+        printArr(arr);
+    }
+
+    public static void heapSort(int[] arr) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        for (int value : arr) queue.add(value);
+
+        int i = 0;
+        while (!queue.isEmpty())
+            arr[i++] = queue.poll();
+    }
 }
