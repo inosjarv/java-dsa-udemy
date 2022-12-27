dcf61fc5488511ca0cc4c3fd9ae77bf50e36f219
Vraj Soni <vrajds8725@gmail.com>
2022-12-27 16:16:50 +0530

    added bellmen-ford algorithm

--- src/main/java/io/dsa/weightedgraph/WeightedGraph.java
+++ src/main/java/io/dsa/weightedgraph/WeightedGraph.java
@@ -3,8 +3,7 @@ package io.dsa.weightedgraph;
 import java.util.List;
 import java.util.PriorityQueue;
 
-import static io.dsa.Colors.CYAN;
-import static io.dsa.Colors.colorPrint;
+import static io.dsa.Colors.*;
 
 public class WeightedGraph {
     List<WeightedNode> nodeList;
@@ -14,8 +13,13 @@ public class WeightedGraph {
     }
 
     public static void main(String[] args) {
+        colorPrintln("\nDijkstra\n", YELLOW);
         WeightedGraph weightedGraph = WeightedGraphBuilder.buildWeightedGraph();
         weightedGraph.dijkstra(weightedGraph.nodeList.get(0));
+
+        colorPrintln("\nBellmen-Ford\n", YELLOW);
+        WeightedGraph weightedGraph1 = WeightedGraphBuilder.buildWeightedGraph();
+        weightedGraph1.bellmanFord(weightedGraph1.nodeList.get(0));
     }
 
     private static void pathPrint(WeightedNode node) {
@@ -57,5 +61,40 @@ public class WeightedGraph {
         WeightedNode second = nodeList.get(j);
         first.neighbors.add(second);
         first.weightMap.put(second, distance);
+    }
+
+    public void bellmanFord(WeightedNode sourceNode) {
+        sourceNode.distance = 0;
+
+        for (int i = 0; i < nodeList.size(); i++) {
+            for (WeightedNode currentNode : nodeList) {
+                for (WeightedNode neighbor : currentNode.neighbors) {
+                    if (neighbor.distance > currentNode.distance + currentNode.weightMap.get(neighbor)) {
+                        neighbor.distance = currentNode.distance + currentNode.weightMap.get(neighbor);
+                        neighbor.parent = currentNode;
+                    }
+                }
+            }
+        }
+
+        System.out.println("Checking for Negative Cycle..");
+        for (WeightedNode currentNode : nodeList) {
+            for (WeightedNode neighbor : currentNode.neighbors) {
+                if (neighbor.distance > currentNode.distance + currentNode.weightMap.get(neighbor)) {
+                    colorPrintln("Negative Cycle Found...", BLUE);
+                    colorPrintln("Vertex Name: " + neighbor, CYAN);
+                    colorPrintln("Old Cost: " + neighbor.distance, CYAN);
+                    neighbor.distance = currentNode.distance + currentNode.weightMap.get(neighbor);
+                    neighbor.parent = currentNode;
+                    colorPrintln("New Cost: " + neighbor.distance, CYAN);
+                }
+            }
+        }
+
+        for (WeightedNode nodeToCheck : nodeList) {
+            colorPrint("Node " + nodeToCheck + ", distance: " + nodeToCheck.distance + ", Path: ", CYAN);
+            pathPrint(nodeToCheck);
+            System.out.println();
+        }
     }
 }
