0d6c5b817fd8c19646c66995c641a7713368485c
Vraj Soni <vrajds8725@gmail.com>
2022-12-27 09:09:08 +0530

    refactoring

--- src/main/java/io/dsa/graphmatrix/GraphAdjacencyMatrix.java
+++ src/main/java/io/dsa/graphmatrix/GraphAdjacencyMatrix.java
@@ -3,6 +3,7 @@ package io.dsa.graphmatrix;
 import java.util.*;
 
 import static io.dsa.Colors.*;
+import static io.dsa.graphmatrix.GraphAdjacencyMatrixBuilder.*;
 
 class GraphNode {
     String name;
@@ -39,49 +40,6 @@ public class GraphAdjacencyMatrix {
 
         GraphAdjacencyMatrix graph2 = directedGraphBuilder();
         graph2.topologicalSort();
-    }
-
-    private static GraphAdjacencyMatrix undirectedGraphBuilder() {
-        List<GraphNode> nodeList = new ArrayList<>();
-        nodeList.add(new GraphNode("A", 0));
-        nodeList.add(new GraphNode("B", 1));
-        nodeList.add(new GraphNode("C", 2));
-        nodeList.add(new GraphNode("D", 3));
-        nodeList.add(new GraphNode("E", 4));
-
-        GraphAdjacencyMatrix graph = new GraphAdjacencyMatrix(nodeList);
-        graph.addUndirectedEdge(0, 1);
-        graph.addUndirectedEdge(0, 2);
-        graph.addUndirectedEdge(0, 3);
-        graph.addUndirectedEdge(1, 4);
-        graph.addUndirectedEdge(2, 3);
-        graph.addUndirectedEdge(3, 4);
-
-        return graph;
-    }
-
-    private static GraphAdjacencyMatrix directedGraphBuilder() {
-        List<GraphNode> nodeList = new ArrayList<>();
-        nodeList.add(new GraphNode("A", 0));
-        nodeList.add(new GraphNode("B", 1));
-        nodeList.add(new GraphNode("C", 2));
-        nodeList.add(new GraphNode("D", 3));
-        nodeList.add(new GraphNode("E", 4));
-        nodeList.add(new GraphNode("F", 5));
-        nodeList.add(new GraphNode("G", 6));
-        nodeList.add(new GraphNode("H", 7));
-
-        GraphAdjacencyMatrix graph = new GraphAdjacencyMatrix(nodeList);
-        graph.addDirectedEdge(0, 2);
-        graph.addDirectedEdge(1, 2);
-        graph.addDirectedEdge(1, 3);
-        graph.addDirectedEdge(2, 4);
-        graph.addDirectedEdge(3, 5);
-        graph.addDirectedEdge(4, 5);
-        graph.addDirectedEdge(4, 7);
-        graph.addDirectedEdge(5, 6);
-
-        return graph;
     }
 
     public void addUndirectedEdge(int i, int j) {
